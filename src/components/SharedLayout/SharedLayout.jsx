ac9041f8a839e625f3d195534d00578d24eaeb57
Maksym Dorohokuplya <dorogmaks@gmail.com>
2022-11-15 22:52:48 +0200

    styled SharedLayout

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,19 +1,33 @@
-import { NavLink, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { GlobalStyles } from 'components/GlobalStyles';
+import {
+  HeaderStyled,
+  Logo,
+  LogoLink,
+  NavigationContainer,
+  NavigationLink,
+  NavigationStyled,
+} from './SharedLayout.styled';
+import { Container } from 'components/Shared/Container.styled';
 
 export const SharedLayout = () => (
   <>
     <GlobalStyles />
-    <header>
-      <div>
-        <nav>
-          <NavLink to="/" end>
-            Home
-          </NavLink>
-          <NavLink to="/movies">Movies</NavLink>
-        </nav>
-      </div>
-    </header>
+    <HeaderStyled>
+      <Container>
+        <NavigationStyled>
+          <LogoLink to="/">
+            <Logo>The Movies</Logo>
+          </LogoLink>
+          <NavigationContainer>
+            <NavigationLink to="/" end>
+              Home
+            </NavigationLink>
+            <NavigationLink to="/movies">Movies</NavigationLink>
+          </NavigationContainer>
+        </NavigationStyled>
+      </Container>
+    </HeaderStyled>
     <main>
       <Outlet />
     </main>
--- src/components/SharedLayout/SharedLayout.styled.js
+++ src/components/SharedLayout/SharedLayout.styled.js
@@ -1,21 +1,51 @@
-import { NavLink, Outlet } from 'react-router-dom';
-import { GlobalStyles } from 'components/GlobalStyles';
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
 
-export const SharedLayout = () => (
-  <>
-    <GlobalStyles />
-    <header>
-      <div>
-        <nav>
-          <NavLink to="/" end>
-            Home
-          </NavLink>
-          <NavLink to="/movies">Movies</NavLink>
-        </nav>
-      </div>
-    </header>
-    <main>
-      <Outlet />
-    </main>
-  </>
-);
+export const HeaderStyled = styled.header`
+  border-bottom: 1px solid rgb(204, 204, 204);
+  box-shadow: rgb(0 0 0 / 12%) 0px 1px 1px, rgb(0 0 0 / 6%) 0px 4px 4px,
+    rgb(0 0 0 / 16%) 1px 4px 6px;
+`;
+
+export const NavigationStyled = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const LogoLink = styled(NavLink)`
+  text-decoration: none;
+  color: inherit;
+  transition: color 250ms ease 0s;
+  &:hover {
+    color: rgb(110, 110, 110);
+  }
+`;
+
+export const Logo = styled.h1`
+  margin: 0;
+  padding: 0;
+`;
+
+export const NavigationContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const NavigationLink = styled(NavLink)`
+  padding: 16px 0px;
+  font-weight: 600;
+  color: inherit;
+  text-decoration: none;
+  transition: color 250ms ease 0s;
+  &:not(:last-child) {
+    margin-right: 16px;
+  }
+  &.active {
+    color: rgb(34, 139, 34);
+  }
+  &:hover:not(.active),
+  &:focus:not(.active) {
+    color: rgb(110, 110, 110);
+  }
+`;
